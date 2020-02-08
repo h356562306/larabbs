bd1611c6e15fa9a1385b2a5e1b65dd5746b19db3
huangguangrong <356562306@qq.com>
2020-02-08 21:15:55 +0800

    认证后消息提醒

--- resources/views/shared/_message.blade.php
+++ resources/views/shared/_message.blade.php
@@ -1,7 +1,7 @@
 @foreach(['success','danger','warning','info'] as $msg)
   @if(session()->has($msg))
     <div class="flash-message">
-      <p class="alert alert-{{$mag}}">
+      <p class="alert alert-{{$msg}}">
         {{session()->get($msg)}}
       </p>
     </div>
