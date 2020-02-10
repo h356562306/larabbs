2714e8432fba72655e14d659987e90cfda71dc35
huangguangrong <356562306@qq.com>
2020-02-10 20:52:46 +0800

    话题排序

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -10,8 +10,8 @@ class CategoriesController extends Controller
 {
     //
 
-    public function show(Category $category){
-        $topics = Topic::where('category_id',$category->id)->paginate(20);
+    public function show(Category $category,Request $request,Topic $topic){
+        $topics = Topic::withOrder($request->order)->where('category_id',$category->id)->paginate(20);
         return view('topics.index',compact("topics",'category'));
     }
 }
