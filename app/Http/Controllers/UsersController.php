ad7b9157f60383b8bc1a395ea7e8a02dcec334b6
huangguangrong <356562306@qq.com>
2020-02-10 11:50:34 +0800

    权限控制

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,15 +10,23 @@ use Illuminate\Http\Request;
 class UsersController extends Controller
 {
     //
+
+    public function __construct()
+    {
+        $this->middleware('auth',['except'=>'show']);
+    }
+
     public function show(User $user){
         return view("users.show",compact("user"));
     }
 
     public function edit(User $user){
+        $this->authorize("update",$user);
         return view("users.edit",compact("user"));
     }
 
     public function update(UserRequest $userRequest,ImageUploadHandler $imageUploadHandler,User $user){
+        $this->authorize("update",$user);
         $data = $userRequest->all();
         if ($userRequest->avatar){
             $result = $imageUploadHandler->save($userRequest->avatar,'avatar',$user->id,416);
